6c12d4649fc0904b27cfd612405ac5e8e96febf5
Gabriel <162498294+gabrielms040@users.noreply.github.com>
2025-01-19 21:35:06 -0300

    update

--- src/componentes/banner/index.jsx
+++ src/componentes/banner/index.jsx
@@ -3,7 +3,7 @@ import "./banner.css"
 const Banner = () => {
     return ( 
         <header className="Banner">
-        <img src = "./public/img/capa.png" alt="Banner"></img>
+        <img src = "/img/capa.png" alt="Banner"></img>
         </header>
     )
 }
